2bc71f3d830a6f4073a0907e083999a4be9100d9
HILMI <hlmozcelik1@gmail.com>
2022-05-19 16:45:20 +0300

    Planet2 features added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,6 +22,13 @@ public class Main {
         FacX.radius="738y4932749km";
         FacX.size="9999999999m";
 
+
+        //        adding features for Planet2
+        Planet2 Olso = new Planet2();
+        Olso.name="Ozhan";
+        Olso.size="694254km";
+        Olso.color="pink";
+
     }
 }
 
